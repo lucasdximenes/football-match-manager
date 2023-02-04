1a0af99e65b435c336bebf7159644467aeca562c
Lucas Ximenes <lucasdximenes@gmail.com>
2023-02-04 20:49:08 -0300

    feat: :rocket: create match GET route /matches to return all matches

---
+++ app/backend/src/routes/Match.routes.ts
@@ -0,0 +1,21 @@
+import { Router } from 'express';
+import Match from '../database/models/Match';
+import MatchController from '../controllers/Match.controllers';
+import MatchServices from '../services/Match.services';
+import MatchRepository from '../repositories/sequelize/Match.repository';
+
+export default class MatchRoutes {
+  public router: Router;
+  private _matchController: MatchController;
+  private _matchServices: MatchServices;
+  private _matchRepository: MatchRepository;
+
+  constructor() {
+    this.router = Router();
+    this._matchRepository = new MatchRepository(Match);
+    this._matchServices = new MatchServices(this._matchRepository);
+    this._matchController = new MatchController(this._matchServices);
+
+    this.router.get('/', this._matchController.getAll);
+  }
+}
--- app/backend/src/routes/index.ts
+++ app/backend/src/routes/index.ts
@@ -1,18 +1,22 @@
 import { Router } from 'express';
 import UserRoutes from './User.routes';
 import TeamRoutes from './Team.routes';
+import MatchRoutes from './Match.routes';
 
 export default class Routes {
   public router: Router;
   private _userRoutes: UserRoutes;
   private _teamRoutes: TeamRoutes;
+  private _matchRoutes: MatchRoutes;
 
   constructor() {
     this.router = Router();
     this._userRoutes = new UserRoutes();
     this._teamRoutes = new TeamRoutes();
+    this._matchRoutes = new MatchRoutes();
 
     this.router.use('/login', this._userRoutes.router);
     this.router.use('/teams', this._teamRoutes.router);
+    this.router.use('/matches', this._matchRoutes.router);
   }
 }
