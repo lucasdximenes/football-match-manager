2338b0c6aa59e6cfaff2dd687ce2ddd6a90a3558
Lucas Ximenes <lucasdximenes@gmail.com>
2023-02-03 23:08:36 -0300

    feat: :rocket: create `tokenUtils` for jwt operations

---
+++ app/backend/src/utils/tokenUtils.ts
@@ -0,0 +1,22 @@
+import * as jwt from 'jsonwebtoken';
+import { unauthorized } from '@hapi/boom';
+import { JwtPayload } from 'jsonwebtoken';
+
+const { JWT_SECRET } = process.env;
+
+export default class tokenUtils {
+  static generateToken(payload: object): string {
+    return jwt.sign(payload, JWT_SECRET as string, {
+      expiresIn: '1d',
+    });
+  }
+
+  static verifyToken(token: string): JwtPayload {
+    try {
+      const decoded = jwt.verify(token, JWT_SECRET as string);
+      return decoded as JwtPayload;
+    } catch (err) {
+      throw unauthorized('Invalid token');
+    }
+  }
+}
