8507e00651a0c8d26bdc357738f8fb3efd2df512
Lucas Ximenes <lucasdximenes@gmail.com>
2023-02-04 17:28:08 -0300

    feat: :rocket: create Team route GET `/teams` to getAll teams

---
+++ app/backend/src/routes/Team.routes.ts
@@ -0,0 +1,21 @@
+import { Router } from 'express';
+import Team from '../database/models/Team';
+import TeamController from '../controllers/Team.controllers';
+import TeamServices from '../services/Team.services';
+import TeamRepository from '../repositories/sequelize/Team.repository';
+
+export default class TeamRoutes {
+  public router: Router;
+  private _teamController: TeamController;
+  private _teamServices: TeamServices;
+  private _teamRepository: TeamRepository;
+
+  constructor() {
+    this.router = Router();
+    this._teamRepository = new TeamRepository(Team);
+    this._teamServices = new TeamServices(this._teamRepository);
+    this._teamController = new TeamController(this._teamServices);
+
+    this.router.get('/', this._teamController.getAll);
+  }
+}
--- app/backend/src/routes/index.ts
+++ app/backend/src/routes/index.ts
@@ -1,13 +1,18 @@
 import { Router } from 'express';
 import UserRoutes from './User.routes';
+import TeamRoutes from './Team.routes';
 
 export default class Routes {
   public router: Router;
   private _userRoutes: UserRoutes;
+  private _teamRoutes: TeamRoutes;
 
   constructor() {
     this.router = Router();
     this._userRoutes = new UserRoutes();
+    this._teamRoutes = new TeamRoutes();
+
     this.router.use('/login', this._userRoutes.router);
+    this.router.use('/teams', this._teamRoutes.router);
   }
 }
