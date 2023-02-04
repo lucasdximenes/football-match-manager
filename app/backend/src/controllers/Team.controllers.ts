64fe6c1228baf7305b58a6083f4d23ecd8aaf68c
Lucas Ximenes <lucasdximenes@gmail.com>
2023-02-04 17:48:39 -0300

    feat: :rocket: implement `getById` function

--- app/backend/src/controllers/Team.controllers.ts
+++ app/backend/src/controllers/Team.controllers.ts
@@ -7,6 +7,12 @@ export default class TeamControllers implements ITeamControllers {
 
   public getAll = async (_req: Request, res: Response): Promise<Response> => {
     const teams = await this.teamService.getAll();
-    return res.status(200).json({ teams });
+    return res.status(200).json(teams);
+  };
+
+  public getById = async (req: Request, res: Response): Promise<Response> => {
+    const { id } = req.params;
+    const team = await this.teamService.getById(Number(id));
+    return res.status(200).json(team);
   };
 }
