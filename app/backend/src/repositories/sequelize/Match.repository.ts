8b2aced13911de28672ff555d4a3402d59b9a34d
Lucas Ximenes <lucasdximenes@gmail.com>
2023-02-05 14:20:33 -0300

    feat: :rocket: fix Matches repository interfaces and create `getByProgress` function

--- app/backend/src/repositories/sequelize/Match.repository.ts
+++ app/backend/src/repositories/sequelize/Match.repository.ts
@@ -1,10 +1,11 @@
 import Match from '../../database/models/Match';
+import Matches from '../../interfaces/Match.interface';
 import IMatchRepository from '../interfaces/IMatchRepository';
 
 export default class MatchRepository implements IMatchRepository {
   constructor(private matchModel: typeof Match) {}
 
-  public async getAll(): Promise<Match[]> {
+  public async getAll(): Promise<Matches[]> {
     const matches = await this.matchModel.findAll({
       include: [
         {
@@ -17,6 +18,23 @@ export default class MatchRepository implements IMatchRepository {
         },
       ],
     });
-    return matches;
+    return matches as unknown as Matches[];
+  }
+
+  public async getByProgress(inProgress: boolean): Promise<Matches[]> {
+    const matches = await this.matchModel.findAll({
+      where: { inProgress },
+      include: [
+        {
+          association: 'homeTeam',
+          attributes: ['teamName'],
+        },
+        {
+          association: 'awayTeam',
+          attributes: ['teamName'],
+        },
+      ],
+    });
+    return matches as unknown as Matches[];
   }
 }
