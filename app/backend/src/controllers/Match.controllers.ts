23ab5f13d7b3e5cec03e95cc3ae1c84908b9ecfe
Lucas Ximenes <lucasdximenes@gmail.com>
2023-02-05 14:21:41 -0300

    feat: :rocket: create `getByProgress` function in Match controller

--- app/backend/src/controllers/Match.controllers.ts
+++ app/backend/src/controllers/Match.controllers.ts
@@ -6,7 +6,21 @@ export default class MatchControllers implements IMatchControllers {
   constructor(private matchServices: IMatchServices) {}
 
   public getAll = async (req: Request, res: Response): Promise<Response> => {
+    const { inProgress } = req.query;
+    if (inProgress) {
+      return this.getByProgress(req, res);
+    }
     const matches = await this.matchServices.getAll();
+    return res.status(200).json(matches);
+  };
+
+  public getByProgress = async (
+    req: Request,
+    res: Response,
+  ): Promise<Response> => {
+    const { inProgress } = req.query;
+    const inProgressBool = inProgress === 'true';
+    const matches = await this.matchServices.getByProgress(inProgressBool);
     return res.status(200).json(matches);
   };
 }
--- app/backend/src/controllers/interfaces/IMatchControllers.ts
+++ app/backend/src/controllers/interfaces/IMatchControllers.ts
@@ -2,4 +2,5 @@ import { Request, Response } from 'express';
 
 export default interface IMatchControllers {
   getAll(req: Request, res: Response): Promise<Response>;
+  getByProgress(req: Request, res: Response): Promise<Response>;
 }
