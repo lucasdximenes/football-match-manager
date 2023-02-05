50f6d039098c00f6687cc9fcb83e410e3406586d
Lucas Ximenes <lucasdximenes@gmail.com>
2023-02-05 15:33:18 -0300

    feat: :rocket: add POST /matches to create a new match

--- app/backend/src/routes/Match.routes.ts
+++ app/backend/src/routes/Match.routes.ts
@@ -1,26 +1,48 @@
 import { Router } from 'express';
 import Match from '../database/models/Match';
+import Team from '../database/models/Team';
 import MatchValidations from '../middlewares/validations/Match.validations';
 import MatchController from '../controllers/Match.controllers';
 import MatchServices from '../services/Match.services';
 import MatchRepository from '../repositories/sequelize/Match.repository';
+import TeamServices from '../services/Team.services';
+import TeamRepository from '../repositories/sequelize/Team.repository';
+import UserValidations from '../middlewares/validations/User.validations';
 
 export default class MatchRoutes {
   public router: Router;
   private _matchController: MatchController;
   private _matchServices: MatchServices;
   private _matchRepository: MatchRepository;
+  private _teamServices: TeamServices;
+  private _teamRepository: TeamRepository;
 
   constructor() {
     this.router = Router();
+    this._teamRepository = new TeamRepository(Team);
+    this._teamServices = new TeamServices(this._teamRepository);
     this._matchRepository = new MatchRepository(Match);
-    this._matchServices = new MatchServices(this._matchRepository);
+    this._matchServices = new MatchServices(
+      this._matchRepository,
+      this._teamServices,
+    );
     this._matchController = new MatchController(this._matchServices);
 
+    this._initRoutes();
+  }
+
+  private _initRoutes(): void {
     this.router.get(
       '/',
       MatchValidations.validateMatchInProgressQuery,
       this._matchController.getAll,
+    );
+
+    this.router.post(
+      '/',
+      UserValidations.validateToken,
+      MatchValidations.validateNewMatchBody,
+      this._matchController.createMatch,
     );
   }
 }
