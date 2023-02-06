9f0dba6dcee63221dace1cc24ea1a4e74b593a0a
Lucas Ximenes <lucasdximenes@gmail.com>
2023-02-06 17:38:09 -0300

    feat: :rocket: create GET /leaderboard routes

--- app/backend/src/routes/index.ts
+++ app/backend/src/routes/index.ts
@@ -2,21 +2,25 @@ import { Router } from 'express';
 import UserRoutes from './User.routes';
 import TeamRoutes from './Team.routes';
 import MatchRoutes from './Match.routes';
+import LeaderboardRoutes from './Leaderboard.routes';
 
 export default class Routes {
   public router: Router;
   private _userRoutes: UserRoutes;
   private _teamRoutes: TeamRoutes;
   private _matchRoutes: MatchRoutes;
+  private _leaderboardRoutes: LeaderboardRoutes;
 
   constructor() {
     this.router = Router();
     this._userRoutes = new UserRoutes();
     this._teamRoutes = new TeamRoutes();
     this._matchRoutes = new MatchRoutes();
+    this._leaderboardRoutes = new LeaderboardRoutes();
 
     this.router.use('/login', this._userRoutes.router);
     this.router.use('/teams', this._teamRoutes.router);
     this.router.use('/matches', this._matchRoutes.router);
+    this.router.use('/leaderboard', this._leaderboardRoutes.router);
   }
 }
